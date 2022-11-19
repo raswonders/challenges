7c3c21f2df6aa22178eb409a6e0caf9cea447b02
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-11-19 10:11:59 +0100

    add new solution for String incrementor

--- codewars/js/regular expressions/String incrementer.js
+++ codewars/js/regular expressions/String incrementer.js
@@ -1,11 +1,3 @@
 function incrementString(str) {
-  let strInc = str.replace(/(0*)\d*$/, (digits, zeroes) => {
-    let lastDigit = digits.slice(-1);
-    
-    if (lastDigit === "9" || lastDigit === "0") zeroes = zeroes.slice(0, -1);
-    
-    return zeroes + String(Number(digits) + 1);
-  })
-  
-  return strInc === str ? strInc + "1" : strInc;
+  return str.replace(/([0-8]|\d?9+)?$/, suffix => suffix ? +suffix + 1 : 1);
 }
