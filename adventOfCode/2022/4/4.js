11e0951288c9eecb2df8a54c9ce59a36a5d6e49d
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-12-04 19:05:46 +0100

    add AdventOfCode 2022 challenge 4 part2

--- adventOfCode/2022/4/4.js
+++ adventOfCode/2022/4/4.js
@@ -8,14 +8,29 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
 
   let assignments = data.split('\n').map(ass => ass.split(',').map(range => range.split('-').map(str => Number(str))))
 
-  let total = 0;
-  assignments.map(ass => {
-    let range1 = ass[0];
-    let range2 = ass[1];
+  let totalContained = 0;
+  let totalOverlaped = 0;
+  for (assignemnt of assignments) {
+    let range1 = assignemnt[0];
+    let range2 = assignemnt[1];
 
-    if (range1[0] >= range2[0] && range1[1] <= range2[1]) total++;
-    else if (range1[0] <= range2[0] && range1[1] >= range2[1]) total++;
-  })
+    if (range1[0] >= range2[0] && range1[1] <= range2[1]) {
+      totalContained++;
+      continue;
+    } else if (range1[0] <= range2[0] && range1[1] >= range2[1]) {
+      totalContained++;
+      continue;
+    }
 
-  console.log(total)
+    if (range1[0] >= range2[0] && range1[0] <= range2[1]) {
+      totalOverlaped++;
+    } else if (range1[1] >= range2[0] && range1[1] <= range2[1]) {
+      totalOverlaped++;
+    }
+  }
+
+  totalOverlaped += totalContained;
+
+  console.log("no. contained", totalContained)
+  console.log("no. overlaped", totalOverlaped)
 })
