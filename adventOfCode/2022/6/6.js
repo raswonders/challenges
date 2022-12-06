2cc9d42f4d38b3cc336d9f21e09d5b93e8e73c09
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-12-06 15:48:48 +0100

    add adventOfCode challenge 6 part2

--- adventOfCode/2022/6/6.js
+++ adventOfCode/2022/6/6.js
@@ -1,33 +1,52 @@
 const fs = require("fs");
 
-fs.readFile("./input.txt", "utf8", (err, signal) => {
+fs.readFile("./input.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
+  let firstPacket = getStartOfPacket(data);
+  let firstMessage = getStartOfMessage(data);
+
+  console.log(`Packet marker recognized after ${firstPacket} characters`);
+  console.log(`Message marker recognized after ${firstMessage} characters`);
+});
+
+function getStartOfPacket(signal) {
   let start = 0;
   let end = 4;
   let slice;
+
   while ((slice = signal.slice(start, end))) {
-    if (isMarker(slice)) {
-      console.log(`Marker recognized after ${end} characters`);
-      break;
-    }
+    if (isMarker(slice)) return end;
 
     start++;
     end++;
   }
-});
+}
 
-function isMarker(slice) {
-  if (slice.length !== 4) return false;
+function getStartOfMessage(signal) {
+  let start = 0;
+  let end = 14;
+  let slice;
 
-  let chars = slice.split('');
+  while ((slice = signal.slice(start, end))) {
+    if (isMarker(slice, 14)) return end;
+
+    start++;
+    end++;
+  }
+}
+
+function isMarker(slice, size=4) {
+  if (slice.length !== size) return false;
+
+  let chars = slice.split("");
 
   for (c of chars) {
-    if (c === '^' || c === '-') c = `\${c}`;
-    let regex = new RegExp(`[${c}]`, 'g');
+    if (c === "^" || c === "-") c = `\${c}`;
+    let regex = new RegExp(`[${c}]`, "g");
     let matches = slice.match(regex) || [];
 
     if (matches.length > 1) return false;
