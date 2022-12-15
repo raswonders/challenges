ae4b3cecdfba08b26d27e791769e0c44fd8ffc88
Rastislav Hepner <rastislav.hepner@gmail.com>
2022-12-15 17:49:43 +0100

    add adventOfCode 2022 challenge 9 part2

--- adventOfCode/2022/9/9.js
+++ adventOfCode/2022/9/9.js
@@ -1,46 +1,87 @@
 const fs = require("fs");
 
+let tailPositions = {
+  positions: [[0, 0]],
+
+  hasPosition(node) {
+    return Boolean(
+      this.positions.filter(item => item[0] === node[0] && item[1] === node[1])
+        .length
+    );
+  },
+
+  add(node) {
+    if (this.hasPosition(node)) return;
+    newNode = node.slice();
+    this.positions.push(newNode);
+  },
+
+  increaseX() {
+    this.positions.forEach(node => node[0]++);
+  },
+
+  increaseY() {
+    this.positions.forEach(node => node[1]++);
+  },
+
+  get total() {
+    return this.positions.length;
+  }
+};
+
 class KnotsOnGrid {
   constructor() {
-    this.grid = [['#*']];
-    this.head = [0, 0];
-    this.tail = [0, 0];
+    this.grid = [["#*"]];
+    this.nodes = new Array(10);
+    for (let i = 0; i < 10; i++) {
+      this.nodes[i] = [0, 0];
+    }
+    this.head = this.nodes[0];
+    this.tail = this.nodes[9];
+  }
+
+  increaseX() {
+    this.nodes.forEach(node => node[0]++);
+  }
+
+  increaseY() {
+    this.nodes.forEach(node => node[1]++);
   }
 
   moveHead(vector) {
     // unmark old position
-    this.unmarkHead()
+    this.unmarkHead();
 
     this.head[0] += vector[0];
     this.head[1] += vector[1];
 
-    // if head gets beyond current grid borders -> autoexpand grid 
+    // if head gets beyond current grid borders -> autoexpand grid
     // and sanitize head coordinates
     if (this.head[0] >= this.grid[0].length) this.expandGridRight();
     if (this.head[1] >= this.grid.length) this.expandGridTop();
     if (this.head[0] < 0) {
       this.expandGridLeft();
-      this.head[0]++;
-      this.tail[0]++;
+      this.increaseX();
+      tailPositions.increaseX();
     }
     if (this.head[1] < 0) {
       this.expandGridBot();
-      this.head[1]++;
-      this.tail[1]++;
+      this.increaseY();
+      tailPositions.increaseY();
     }
 
-    this.markHead()
-    this.updateTail();
+    this.markHead();
+    this.updateNodes();
   }
 
   markHead() {
     let value = this.grid[this.head[1]][this.head[0]];
-    this.grid[this.head[1]][this.head[0]] = value + '*';
+    this.grid[this.head[1]][this.head[0]] = value + "*";
   }
 
   unmarkHead() {
     let value = this.grid[this.head[1]][this.head[0]];
-    this.grid[this.head[1]][this.head[0]] = value.replace('*', '')
+    this.grid[this.head[1]][this.head[0]] = value.replace("*", "");
   }
 
   expandGridRight() {
@@ -57,13 +98,13 @@ class KnotsOnGrid {
 
   expandGridTop() {
     let rowLen = this.grid[0].length;
-    let newRow = Array(rowLen).fill('.')
+    let newRow = Array(rowLen).fill(".");
     this.grid.push(newRow);
   }
 
   expandGridBot() {
     let rowLen = this.grid[0].length;
-    let newRow = Array(rowLen).fill('.')
+    let newRow = Array(rowLen).fill(".");
     this.grid.unshift(newRow);
   }
 
@@ -75,29 +116,44 @@ class KnotsOnGrid {
 
     let x = xDiff > 0 ? Math.ceil(xDiff) : Math.floor(xDiff);
     let y = yDiff > 0 ? Math.ceil(yDiff) : Math.floor(yDiff);
-  
+
     let vector = [x, y];
 
     this.moveTail(vector);
   }
 
-  moveTail(vector) {
-    this.tail[0] += vector[0];
-    this.tail[1] += vector[1];
+  updateNodes() {
+    for (let i = 1; i < 10; i++) {
+      let prev = this.nodes[i - 1];
+      let curr = this.nodes[i];
 
-    // y-axis is inversed
-    this.grid[this.tail[1]][this.tail[0]] = '#';
+      if (this.isAroundNode(prev, curr)) return;
+
+      let xDiff = (prev[0] - curr[0]) / 2;
+      let yDiff = (prev[1] - curr[1]) / 2;
+
+      let x = xDiff > 0 ? Math.ceil(xDiff) : Math.floor(xDiff);
+      let y = yDiff > 0 ? Math.ceil(yDiff) : Math.floor(yDiff);
+
+      let vector = [x, y];
+      this.moveNode(i, vector);
+    }
   }
 
-  isAroundHead() {
-    if (Math.abs(this.head[0] - this.tail[0]) > 1) return false;
-    if (Math.abs(this.head[1] - this.tail[1]) > 1) return false;
+  moveNode(nodeIn, vector) {
+    let node = this.nodes[nodeIn];
+    this.grid[node[1]][node[0]] = ".";
+    node[0] += vector[0];
+    node[1] += vector[1];
+    this.grid[node[1]][node[0]] = `${nodeIn}`;
+    if (nodeIn === 9) tailPositions.add(node);
+  }
+
+  isAroundNode(prev, curr) {
+    if (Math.abs(prev[0] - curr[0]) > 1) return false;
+    if (Math.abs(prev[1] - curr[1]) > 1) return false;
 
     return true;
-  }
-
-  getVistedCount() {
-    return this.grid.flat().join("").replace(/\*/g, "").replace(/\./g, "").length;
   }
 }
 
@@ -125,5 +181,5 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
     }
   }
 
-  console.log('visited count:', grid.getVistedCount());
+  console.log("visited count:", tailPositions.total);
 });
