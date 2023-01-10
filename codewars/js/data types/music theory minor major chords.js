065d390e3d962931f1c0fa1bb64acdf9d5c9c175
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-01-10 18:14:05 +0100

    add updated version of codewars challenge - music theory minor major chords

--- codewars/js/data types/music theory minor major chords.js
+++ codewars/js/data types/music theory minor major chords.js
@@ -1,40 +1,19 @@
-notes.getNoteIndex = function(note) {
-  for (let i = 0; i < this.length; i++) {
-    if (note === this[i]) return i;
-    if (Array.isArray(this[i])) {
-      for (let j = 0; j < this[i].length; j++) {
-        if (note === this[i][j]) return i;
-      }
-    }
-  }
-  return -1;
-}
-
 function minorOrMajor(chordStr){
-let chord = chordStr.split(' ');
-if (!isTriad(chord)) return 'Not a chord';
-
-let [first, second, third] = chord; 
-let firstInterval = getInterval(first, second, notes.length);
-let secondInterval = getInterval(second, third, notes.length);
-
-if (firstInterval === 3 && secondInterval === 4) return 'Minor';
-if (firstInterval === 4 && secondInterval === 3) return 'Major';
-return 'Not a chord';
+  const chord = chordStr.split(' ');
+  const isTriad = chord.length === 3;
+  if (!isTriad) return 'Not a chord';
+  
+  let [first, second, third] = chord; 
+  let firstInterval = getInterval(first, second);
+  let secondInterval = getInterval(second, third);
+  
+  if (firstInterval === 3 && secondInterval === 4) return 'Minor';
+  if (firstInterval === 4 && secondInterval === 3) return 'Major';
+  return 'Not a chord';
 }
 
-function getInterval(first, second, length) {
-let firstIndex = notes.getNoteIndex(first);
-let secondIndex = notes.getNoteIndex(second);
-
-let distance = secondIndex - firstIndex;
-let distanceWrapped = length - (firstIndex - secondIndex);
-let distanceUsed = firstIndex > secondIndex ? distanceWrapped : distance;
-
-return (length + distanceUsed) % length;
-}
-
-function isTriad(chord) {
-if (chord.length === 3) return true;
-return false;
+function getInterval(first, second) {
+  let notes = {'C':0,'C#':1,'Db':1,'D':2,'D#':3,'Eb':3,'E':4,'F':5,'F#':6,'Gb':6,'G':7,'G#':8,'Ab':8,'A':9,'A#':10,'Bb':10,'B':11};
+  const scaleSize = 12;
+  return (scaleSize + notes[second] - notes[first]) % scaleSize;
 }
