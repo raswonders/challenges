04e8cd4171554687ce41f3eb0551f3d1ead733a3
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-03-07 15:35:45 +0100

    solve codewars challenge simple fun light bulbs

--- codewars/js/algorithms/Simple fun #219 light bulbs.js
+++ codewars/js/algorithms/Simple fun #219 light bulbs.js
@@ -1,4 +1,15 @@
-function lightBulbs(lights, n) {
-  // INPUT lights: number[], n: number
-  // OUTPUT res: number[]
+function lightBulbs(lights, turns) {
+  lights = lights.map(item => Boolean(item));
+
+  for (let i = 0; i < turns; i++) {
+    lights = lights.map((item, j, arr) => {
+      return (arr[(j + arr.length - 1) % arr.length])
+        ? !item
+        : item;
+    })
+  }
+
+  return lights.map(item => Number(item));
 }
+
+console.log(lightBulbs([0,1,1,0,1,1], 2))
