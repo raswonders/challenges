a8ef9e9b6498d50b6af3a92e46f928bb8e796ebf
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-03-26 13:51:13 +0200

    solution to Add Two Numbers now works even with unsafe integers

--- leetcode/js/Add Two Numbers.js
+++ leetcode/js/Add Two Numbers.js
@@ -1,41 +1,29 @@
-// problem ref. https://leetcode.com/problems/add-two-numbers/
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-
-var addTwoNumbers = function(l1, l2) {
-    let res = getNumber(l1) + get(getNumber(l2);
-    res.split("").reverse().join("");
-    return stringToList(res);
+var addTwoNumbers = function (l1, l2) {
+  let res = String(getNumber(l1) + getNumber(l2));
+  let resRev = res.split("").reverse().join("");
+  return stringToList(resRev);
 };
 
 function listToString(head) {
-    if (head === null) {
-        return "" 
-    }
-
-    return String(head.val) + listToString(head.next);
+  return head ? String(head.val) + listToString(head.next) : "";
 }
 
 function getNumber(head) {
-    let str = listToString(head);
-    let strRev = str.split("").reverse().join("");
-    return Number(strRev)
+  let str = listToString(head);
+  let strRev = str.split("").reverse().join("");
+  return BigInt(strRev);
 }
 
 function stringToList(str) {
-    if (str === null) return null;
+  if (str.length === 0) return null;
 
-    let val = Number(str[0]);
-    return ListNode(val, stringToList(str.slice(1)))
+  let val = BigInt(str[0]);
+  return new ListNode(val, stringToList(str.slice(1)));
 }
+
+console.log(addTwoNumbers(stringToList("243"), stringToList("564")));
