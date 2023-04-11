fd4379a93b919f58f9e0edec9f4b8562499e7e13
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-04-11 10:55:58 +0200

    add leetcode challenge rotate image

---
+++ leetcode/js/rotate image.js
@@ -0,0 +1,18 @@
+function rotate(matrix) {
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = i; j < matrix.length; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  for(let i = 0; i < matrix.length; i++) {
+    matrix[i].reverse(); 
+  }
+
+  return matrix;
+}
+
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]))
+// [[7,4,1],[8,5,2],[9,6,3]]
