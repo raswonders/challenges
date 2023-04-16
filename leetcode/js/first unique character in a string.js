5b34e596be5d74743c689e602256db4760e2fe07
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-04-16 12:03:59 +0200

    add leetcode challenge first unique character in string

---
+++ leetcode/js/first unique character in a string.js
@@ -0,0 +1,24 @@
+function firstUniqChar(s) {
+  for (let i = 0; i < s.length; i++) {
+    const isNonRepeating = s.indexOf(s.charAt(i), s.indexOf(s.charAt(i)) + 1) === -1;
+    if (isNonRepeating) return i;
+  }
+  return -1;
+}
+
+
+
+// EXAMPLES
+console.log(firstUniqChar("leetcode"));
+// 0
+
+console.log(firstUniqChar("aabb"));
+// -1 
+
+console.log(firstUniqChar("aaaaaabbbb"));
+// -1
+
+console.log(firstUniqChar("loveleetcode"));
+// 2
+
+
