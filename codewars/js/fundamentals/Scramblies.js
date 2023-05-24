0b0915b62fcb79b0fe292ef8066ba6a90ad9f17b
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-05-24 16:09:23 +0200

    improve performance of scramblies

--- codewars/js/fundamentals/Scramblies.js
+++ codewars/js/fundamentals/Scramblies.js
@@ -1,15 +1,10 @@
 function scramble(str1, str2) {
-  let letters = str1.split("");
-  let word = str2.split("");
-  for (l of letters) {
-    console.log(l, word)
-    if (word.length === 0) return true;
-    let foundAt = word.indexOf(l);
-    if (foundAt !== -1) {
-      word.splice(foundAt, 1);
-    }
-  }
-  return word.length > 0 ? false : true;
+  let charMap = str1.split("").reduce((acc, curr) => {
+    acc[curr] ? acc[curr]++ : acc[curr] = 1;
+    return acc;
+  }, {});
+  let wordChars = str2.split("");
+  return wordChars.every((c) => --charMap[c] >= 0);
 }
 
 // Examples
