2a2917fd7289d0c025d09b092f217a9c95f3ae00
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-05-26 13:11:19 +0200

    improve performance of binary contiguous array

--- codewars/js/data types/binary contiguous array.js
+++ codewars/js/data types/binary contiguous array.js
@@ -1,20 +1,18 @@
 function binarray(a) {
-  for (let k = a.length; k > 1; k--) {
-    for (let i = 0; i+k <= a.length; i++) {
-      let subArray = a.slice(i, i+k);
-      if (hasEqualOnesToZeros(subArray)) return subArray.length;
+  const balanceSheet = {0: -1};
+  let balance = 0
+  let maxlen = 0;
+  for (let i=0; i < a.length; i++) {
+    if (a[i] === 1) balance++;
+    else balance--;
+    if (balance in balanceSheet) {
+      const len = i - balanceSheet[balance];
+      maxlen = len > maxlen ? len : maxlen;
+    } else {
+      balanceSheet[balance] = i;
     }
   }
-  return 0;
-}
-
-function hasEqualOnesToZeros(arr) {
-  let ones = 0;
-  let zeroes = 0;
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] === 1 ? ones++ : zeroes++;
-  }
-  return ones === zeroes;
+  return maxlen;
 }
 
 // EXAMPLES
