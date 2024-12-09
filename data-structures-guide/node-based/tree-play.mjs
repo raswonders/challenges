911137ce9bee9bee853f8c89ad1af26423f2c972
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-09 20:30:30 +0100

    add binary search tree

---
+++ data-structures-guide/node-based/tree-play.mjs
@@ -0,0 +1,13 @@
+import { deleteNode, insert, TreeNode } from "./tree.mjs";
+import assert from "node:assert";
+const sample = [47, 4, 0, 82, 92, 42, 52, 93, 86, 68];
+const root = new TreeNode(50);
+for (let val of sample) {
+  insert(val, root);
+}
+
+console.log(root);
+deleteNode(0, root);
+console.log(root);
+
+
---
+++ data-structures-guide/node-based/tree.mjs
@@ -0,0 +1,108 @@
+export class TreeNode {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+export function search(searchValue, node) {
+  if (!node || node.value === searchValue) {
+    return node;
+  }
+
+  if (node.value < searchValue) {
+    return search(searchValue, node.right);
+  } else if (node.value > searchValue) {
+    return search(searchValue, node.left);
+  }
+}
+
+export function insert(value, node) {
+  if (value < node.value) {
+    if (!node.left) {
+      node.left = new TreeNode(value);
+    } else {
+      insert(value, node.left);
+    }
+  } else {
+    if (!node.right) {
+      node.right = new TreeNode(value);
+    } else {
+      insert(value, node.right);
+    }
+  }
+}
+
+export function deleteNode(value, rootNode) {
+  let currNode = rootNode;
+  let targetNode = null;
+  let parentNode = null;
+
+  while (currNode) {
+    if (currNode.value === value) {
+      targetNode = currNode;
+      break;
+    } else if (value < currNode.value) {
+      parentNode = currNode;
+      currNode = currNode.left;
+    } else if (value > currNode.value) {
+      parentNode = currNode;
+      currNode = currNode.right;
+    }
+  }
+
+  // value not found
+  if (!targetNode) return null;
+
+  const childrenCount = (targetNode.left ? 1 : 0) + (targetNode.right ? 1 : 0);
+  switch (childrenCount) {
+    case 0:
+      if (!parentNode) {
+        targetNode = null;
+      } else if (targetNode === parentNode.left) {
+        parentNode.left = null;
+      } else if (targetNode === parentNode.right) {
+        parentNode.right = null;
+      }
+      return targetNode;
+    case 1:
+      const targetChild = targetNode.left || targetNode.right;
+      if (!parentNode) {
+        targetNode.value = targetChild.value;
+        targetNode.left = targetChild.left;
+        targetNode.right = targetChild.right;
+      } else if (targetNode === parentNode.left) {
+        parentNode.left = targetChild;
+      } else if (targetNode === parentNode.right) {
+        parentNode.right = targetChild;
+      }
+      return targetChild;
+    case 2:
+      return replaceWithSucc(targetNode);
+  }
+}
+
+function replaceWithSucc(node) {
+  const succNode = node.right;
+  const parentNode = node;
+
+  if (!succNode.left) {
+    node.value = succNode.value;
+    node.right = succNode.right;
+  }
+
+  while (succNode.left) {
+    parentNode = succNode;
+    succNode = succNode.left;
+  }
+
+  if (succNode.right) {
+    parentNode.left = succNode.right;
+  } else {
+    parentNode.left = null;
+  }
+
+  node.value = succNode.value;
+  return succNode;
+}
