0a75d26297ff662089a652467c0a2097519ec07f
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-13 11:26:24 +0100

    add pop() to heap

--- data-structures-guide/node-based/heap.mjs
+++ data-structures-guide/node-based/heap.mjs
@@ -39,4 +39,46 @@ export class Heap {
       newNodeIndex = parentIndex;
     }
   }
+
+  pop() {
+    if (this.data.length === 1) {
+      const deletedValue = this.rootNode();
+      this.data = [];
+      return deletedValue;
+    }
+
+    const deletedValue = this.rootNode();
+    this.data[0] = this.data.pop();
+    let trickleNodeIndex = 0;
+
+    while (this.hasGreaterChild(trickleNodeIndex)) {
+      const greaterChildIndex = this.findGreaterChildIndex(trickleNodeIndex);
+      [this.data[trickleNodeIndex], this.data[greaterChildIndex]] = [
+        this.data[greaterChildIndex],
+        this.data[trickleNodeIndex],
+      ];
+      trickleNodeIndex = greaterChildIndex;
+    }
+  }
+
+  hasGreaterChild(i) {
+    return (
+      this.data[this.leftChildIndex(i)] > this.data[i] ||
+      this.data[this.rightChildIndex(i)] > this.data[i]
+    );
+  }
+
+  findGreaterChildIndex(i) {
+    const rightChildIndex = this.rightChildIndex(i);
+    const leftChildIndex = this.leftChildIndex(i);
+
+    if (
+      rightChildIndex &&
+      this.data[rightChildIndex] > this.data[leftChildIndex]
+    ) {
+      return rightChildIndex;
+    } else {
+      return leftChildIndex;
+    }
+  }
 }
