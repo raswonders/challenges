adc2cd86aec4939800df1ee2dbd57fd16e95ecc5
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-15 11:36:38 +0100

    add autocomplete to trie

--- data-structures-guide/node-based/trie.mjs
+++ data-structures-guide/node-based/trie.mjs
@@ -34,4 +34,24 @@ export class Trie {
     }
     currentNode.children["*"] = null;
   }
+  collectAllWords(words, node = null, word = "") {
+    const currentNode = node || this.root;
+    for (let [key, childNode] of Object.entries(currentNode.children)) {
+      if (key === "*") {
+        words.push(word);
+      } else {
+        this.collectAllWords(words, childNode, word + key);
+      }
+    }
+
+    return words;
+  }
+
+  autocomplete(prefix) {
+    const currentNode = this.search(prefix);
+
+    if (!currentNode) return null;
+
+    return this.collectAllWords([], currentNode);
+  }
 }
