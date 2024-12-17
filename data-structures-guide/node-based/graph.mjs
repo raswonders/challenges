3295aa7b2487c1dcd2467087c572c516fe61f541
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-17 18:26:17 +0100

    add dfs search to graph

--- data-structures-guide/node-based/graph.mjs
+++ data-structures-guide/node-based/graph.mjs
@@ -8,4 +8,32 @@ export class Vertex {
     this.adjacentVertices.push(vertex);
     vertex.addAdjacentVertext.push(this);
   }
+
+  dfsTraverse(vertex, visitedVertices = {}) {
+    visitedVertices[vertex] = true;
+
+    console.log(vertex);
+
+    for (let adjacentVertex of vertex.adjacentVertices) {
+      if (!visitedVertices[adjacentVertex]) {
+        this.dfsTraverse(adjacentVertex, visitedVertices);
+      }
+    }
+  }
+
+  dfs(vertex, searchValue, adjacentVertices) {
+    if (vertex.value === searchValue) {
+      return vertex;
+    }
+
+    adjacentVertices[vertex] = true;
+
+    for (let adjacentVertex of vertex.adjacentVertices) {
+      if (!visitedVertices[adjacentVertex]) {
+        return dfs(vertex, searchValue, adjacentVertices);
+      }
+    }
+
+    return null;
+  }
 }
