049f1f0e0a49b5ef523bd704ced4c673c90b7c32
Rastislav Hepner <rastislav.hepner@gmail.com>
2025-01-28 15:58:29 +0100

    neetcode: binary-search O(1) memory complexity

--- neetcode/binary-search.ts
+++ neetcode/binary-search.ts
@@ -1,18 +1,22 @@
 import { expect, test } from "bun:test";
 
-function search(nums, target, offset = 0) {
-  if (nums.length === 0) {
-    return -1
-  } 
-
-  let mid = Math.floor(nums.length / 2);
-  if (nums[mid] === target) {
-    return mid + offset;
-  } else if (nums[mid] < target) {
-    return search(nums.slice(mid + 1), target, offset + mid + 1);
-  } else if (nums[mid] > target){
-    return search(nums.slice(0, mid), target, offset);
+function binarySearch(nums, target, l, r) {
+  while (l <= r) {
+    let mid = l + Math.floor(Math.abs(l - r) / 2);
+    console.log({nums, target, l, r, mid})
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target > nums[mid]) {
+      l = mid + 1;
+    } else if (target < nums[mid]) {
+      r = mid - 1;
+    }
   }
+  return -1;
+}
+
+function search(nums, target) {
+  return binarySearch(nums, target, 0, nums.length - 1);
 }
 
 test("target is found", () => {
